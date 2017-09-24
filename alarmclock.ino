6312ff74b9e19504dd4d058e3b053b72b252ef69
Peter Buelow <peter.buelow@lifefitness.com>
2017-09-24 10:17:48 -0500

    Closer to done
    Tested with new NtpClientLib content.
    Fixed crash related to old NTP logic.

--- alarmclock.ino
+++ alarmclock.ino
@@ -1,15 +1,32 @@
-#include <adafruit_feather.h>
+
 #include <Adafruit_GFX.h>
 #include <gfxfont.h>
 #include <Adafruit_LEDBackpack.h>
 #include <Time.h>
 #include <TimeLib.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+#include <NtpClientLib.h>
+
+#include "clockbitmaps.h"
 
 #define WLAN_SSID            "LivingRoom"
 #define WLAN_PASS            "Motorazr2V8"
-#define CST_OFFSET        -6
-#define DST_OFFSET        (CST_OFFSET + 1)
+#define CST_OFFSET        -5
+#define DST_OFFSET        (CST_OFFSET - 1)
 #define TIME_BASE_YEAR    2017
+#define ONBOARDLED 2 // Built in LED on ESP-12/ESP-07
+
+WiFiUDP ntpUDP;
 
 const uint8_t _usDSTStart[20] = {12,11,10, 8,14,13,12,10,9,8,14,12,11,10,9,14,13,12,11, 9};
 const uint8_t _usDSTEnd[20] = {5,4,3,1,7,6,5,3,2,1,7,5,4,3,2,7,6,5,4,2};
@@ -18,6 +35,7 @@ Adafruit_8x8matrix matrix1 = Adafruit_8x8matrix();
 Adafruit_8x8matrix matrix2 = Adafruit_8x8matrix();
 Adafruit_8x8matrix matrix3 = Adafruit_8x8matrix();
 Adafruit_8x8matrix matrix4 = Adafruit_8x8matrix();
+
 int lastMinLow;
 int lastMinHigh;
 int lastHourLow;
@@ -25,301 +43,63 @@ int lastHourHigh;
 int count;
 bool g_on;
 bool g_timeSet;
+boolean syncEventTriggered = false; // True if a time even has been triggered
+NTPSyncEvent_t ntpEvent; // Last triggered event
 
-static const uint8_t PROGMEM
-  smile_bmp[] =
-  { B00111100,
-    B01000010,
-    B10100101,
-    B10000001,
-    B10100101,
-    B10011001,
-    B01000010,
-    B00111100 
-  },
-  neutral_bmp[] =
-  { B00111100,
-    B01000010,
-    B10100101,
-    B10000001,
-    B10111101,
-    B10000001,
-    B01000010,
-    B00111100 
-  },
-  frown_bmp[] =
-  { 
-    B00111100,
-    B01000010,
-    B10100101,
-    B10000001,
-    B10011001,
-    B10100101,
-    B01000010,
-    B00111100 
-  };
+bool isDst()
+{
+  bool offset = true;
+  
+  if (month() > 3 && month() < 11) {
+    return false;
+  }
+  else if (month() == 3) {
+    if ((day() == _usDSTStart[year() -  TIME_BASE_YEAR]) && hour() >= 2)
+      offset = false;
+    else if (day() > _usDSTStart[year() -  TIME_BASE_YEAR])
+      offset = false;
+  }
+  else if (month() == 11) {
+    if ((day() == _usDSTEnd[year() -  TIME_BASE_YEAR]) && hour() <=2)
+      offset = false;
+    else if (day() > _usDSTEnd[year() -  TIME_BASE_YEAR])
+      offset = true;
+  }
+  return offset;
+}
 
-static const uint8_t digitoneshifted[] = {
-  B00000000,
-  B00000001,
-  B00000011,
-  B00000001,
-  B00000001,
-  B00000001,
-  B00000001,
-  B00000011
-};
+// Start NTP only after IP network is connected
+void onSTAGotIP(WiFiEventStationModeGotIP ipInfo) 
+{
+  Serial.printf("Got IP: %s\r\n", ipInfo.ip.toString().c_str());
+  NTP.begin("pool.ntp.org", CST_OFFSET, isDst());
+  NTP.setInterval(60*60);
+  digitalWrite(ONBOARDLED, LOW); // Turn on LED
+}
 
-static const uint8_t digittwoshifted[] = {
-  B00000000,
-  B00011100,
-  B00100010,
-  B00000010,
-  B00011100,
-  B00100000,
-  B00100000,
-  B10111110
-};
+// Manage network disconnection
+void onSTADisconnected(WiFiEventStationModeDisconnected event_info) 
+{
+  Serial.printf("Disconnected from SSID: %s\n", event_info.ssid.c_str());
+  Serial.printf("Reason: %d\n", event_info.reason);
+  digitalWrite(ONBOARDLED, HIGH); // Turn off LED
+  //NTP.stop(); // NTP sync can be disabled to avoid sync errors
+}
 
-static const uint8_t digiteleven[] = {
-  B00000000,
-  B01000100,
-  B11001100,
-  B01000100,
-  B01000100,
-  B01000100,
-  B01000100,
-  B11101110
-};
-
-static const uint8_t digitonehigh[] = {
-  B00000000,
-  B00000010,
-  B00000110,
-  B00000010,
-  B00000010,
-  B00000010,
-  B00000010,
-  B00000111
-};
-
-static const uint8_t digitzerocolon[] = {
-  B00000000,
-  B00011100,
-  B00100010,
-  B10100110,
-  B00101010,
-  B10110010,
-  B00100010,
-  B00011100
-};
-
-static const uint8_t digitonecolon[] = {
-  B00000000,
-  B00000100,
-  B00001100,
-  B10000100,
-  B00000100,
-  B10000100,
-  B00000100,
-  B00001110
-};
-
-static const uint8_t digittwocolon[] = {
-  B00000000,
-  B00011100,
-  B00100010,
-  B10000010,
-  B00011100,
-  B10100000,
-  B00100000,
-  B00111110
-};
-
-static const uint8_t digitthreecolon[] = {
-  B00000000,
-  B00111110,
-  B00000010,
-  B10000100,
-  B00001100,
-  B10000010,
-  B00100010,
-  B00011100
-};
-
-static const uint8_t digitfourcolon[] = {
-  B00000000,
-  B00000100,
-  B00001100,
-  B10010100,
-  B00100100,
-  B10111110,
-  B00000100,
-  B00000100
-};
-
-static const uint8_t digitfivecolon[] = {
-  B00000000,
-  B00111110,
-  B00100000,
-  B10111100,
-  B00100010,
-  B10000010,
-  B00100010,
-  B00011100
-};
-
-static const uint8_t digitsixcolon[] = {
-  B00000000,
-  B00001110,
-  B00010000,
-  B10100000,
-  B00111100,
-  B10100010,
-  B00100010,
-  B00011100
-};
-static const uint8_t digitsevencolon[] = {
-  B00000000,
-  B00111110,
-  B00000010,
-  B10000010,
-  B00100100,
-  B10001000,
-  B00010000,
-  B00100000
-};
-
-static const uint8_t digiteightcolon[] = {
-  B00000000,
-  B00011100,
-  B00100010,
-  B10100010,
-  B00011100,
-  B10100010,
-  B00100010,
-  B00011100
-};
-
-static const uint8_t digitninecolon[] = {
-  B00000000,
-  B00011100,
-  B00100010,
-  B10100010,
-  B00011110,
-  B10000010,
-  B00000100,
-  B00111000
-};
-
-static const uint8_t digitzero[] = {
-  B00000000,
-  B00011100,
-  B00100010,
-  B00100110,
-  B00101010,
-  B00110010,
-  B00100010,
-  B00011100
-};
-
-static const uint8_t digitone[] = {
-  B00000000,
-  B00000100,
-  B00001100,
-  B00000100,
-  B00000100,
-  B00000100,
-  B00000100,
-  B00001110
-};
-
-static const uint8_t digittwo[] = {
-  B00000000,
-  B00011100,
-  B00100010,
-  B00000010,
-  B00011100,
-  B00100000,
-  B00100000,
-  B00111110
-};
-
-static const uint8_t digitthree[] = {
-  B00000000,
-  B00111110,
-  B00000010,
-  B00000100,
-  B00001100,
-  B00000010,
-  B00100010,
-  B00011100
-};
-
-static const uint8_t digitfour[] = {
-  B00000000,
-  B00000100,
-  B00001100,
-  B00010100,
-  B00100100,
-  B00111110,
-  B00000100,
-  B00000100
-};
-
-static const uint8_t digitfive[] = {
-  B00000000,
-  B00111110,
-  B00100000,
-  B00111100,
-  B00100010,
-  B00000010,
-  B00100010,
-  B00011100
-};
-
-static const uint8_t digitsix[] = {
-  B00000000,
-  B00001110,
-  B00010000,
-  B00100000,
-  B00111100,
-  B00100010,
-  B00100010,
-  B00011100
-};
-static const uint8_t digitseven[] = {
-  B00000000,
-  B00111110,
-  B00000010,
-  B00000010,
-  B00100100,
-  B00001000,
-  B00010000,
-  B00100000
-};
-
-static const uint8_t digiteight[] = {
-  B00000000,
-  B00011100,
-  B00100010,
-  B00100010,
-  B00011100,
-  B00100010,
-  B00100010,
-  B00011100
-};
-
-static const uint8_t digitnine[] = {
-  B00000000,
-  B00011100,
-  B00100010,
-  B00100010,
-  B00011110,
-  B00000010,
-  B00000100,
-  B00111000
-};
+void processSyncEvent(NTPSyncEvent_t ntpEvent) 
+{
+  if (ntpEvent) {
+    Serial.print("Time Sync error: ");
+    if (ntpEvent == noResponse)
+      Serial.println("NTP server not reachable");
+    else if (ntpEvent == invalidAddress)
+      Serial.println("Invalid NTP server address");
+  }
+  else {
+    Serial.print("Got NTP time: ");
+    Serial.println(NTP.getTimeDateString(NTP.getLastNTPSync()));
+  }
+}
 
 int currentTimeZone()
 {
@@ -344,6 +124,42 @@ int currentTimeZone()
     String debug(String(__FUNCTION__) + ": Current timezone is " + offset);
     Serial.println(debug);
     return offset;
+}
+
+void setMinLowBitmap(int index)
+{
+  switch (index) {
+    case 0:
+      matrix4.drawBitmap(0,0,digitzerolow,8,8,LED_ON);
+      break;
+    case 1:
+      matrix4.drawBitmap(0,0,digitonelow,8,8,LED_ON);
+      break;
+    case 2:
+      matrix4.drawBitmap(0,0,digittwolow,8,8,LED_ON);
+      break;
+    case 3:
+      matrix4.drawBitmap(0,0,digitthreelow,8,8,LED_ON);
+      break;
+    case 4:
+      matrix4.drawBitmap(0,0,digitfourlow,8,8,LED_ON);
+      break;
+    case 5:
+      matrix4.drawBitmap(0,0,digitfivelow,8,8,LED_ON);
+      break;
+    case 6:
+      matrix4.drawBitmap(0,0,digitsixlow,8,8,LED_ON);
+      break;
+    case 7:
+      matrix4.drawBitmap(0,0,digitsevenlow,8,8,LED_ON);
+      break;
+    case 8:
+      matrix4.drawBitmap(0,0,digiteightlow,8,8,LED_ON);
+      break;
+    case 9:
+      matrix4.drawBitmap(0,0,digitninelow,8,8,LED_ON);
+      break;
+  }
 }
 
 void setMinHighBitmap(int index)
@@ -385,9 +201,6 @@ void setMinHighBitmap(int index)
 void setHourLowBitmap(int index)
 {
   switch (index) {
-    case 0:
-      matrix2.drawBitmap(0,0,digitzero,8,8,LED_ON);
-      break;
     case 1:
       matrix2.drawBitmap(0,0,digitone,8,8,LED_ON);
       break;
@@ -415,11 +228,17 @@ void setHourLowBitmap(int index)
     case 9:
       matrix2.drawBitmap(0,0,digitnine,8,8,LED_ON);
       break;
+    case 0:
+    case 10:
+      matrix2.drawBitmap(0,0,digitzero,8,8,LED_ON);
+      matrix1.drawBitmap(0,0,digitoneshifted,8,8,LED_ON);
+      break;
     case 11:
-      matrix2.drawBitmap(0,0,digiteleven,8,8,LED_ON);
+      matrix2.drawBitmap(0,0,digitone,8,8,LED_ON);
+      matrix1.drawBitmap(0,0,digitoneshifted,8,8,LED_ON);
       break;
     case 12:
-      matrix2.drawBitmap(0,0,digittwoshifted,8,8,LED_ON);
+      matrix2.drawBitmap(0,0,digittwo,8,8,LED_ON);
       matrix1.drawBitmap(0,0,digitoneshifted,8,8,LED_ON);
       break;
   }
@@ -453,8 +272,7 @@ void writeTime()
 
   if (lastMinLow != minLow) {
     matrix4.clear();
-    matrix4.setCursor(0,1);
-    matrix4.print(minLow);
+    setMinLowBitmap(minLow);
     matrix4.writeDisplay();
     lastMinLow = minLow;
   }
@@ -463,26 +281,35 @@ void writeTime()
     setMinHighBitmap(minHigh);
     matrix3.writeDisplay();
     lastMinHigh = minHigh;
+    lostConnection();
   }
   if (lastHourLow != hourLow) {
-    setHourLowBitmap(hour());
+    matrix2.clear();
+    matrix1.clear();
+    setHourLowBitmap(newhour);
     matrix2.writeDisplay();
     matrix1.writeDisplay();
     lastHourLow = hourLow;
-  }        
-  if (hour() < 10) {
+    if (hourHigh == 0) {
       matrix1.clear();
-      matrix1.drawBitmap(0,0,smile_bmp,8,8,LED_ON);
+      if (WiFi.status() == WL_CONNECTED) {
+        matrix1.drawBitmap(0,0,smile_bmp,8,8,LED_ON);
+      }
+      else {
+        matrix1.drawBitmap(0,0,frown_bmp,8,8,LED_ON);        
+      }
       matrix1.writeDisplay();
+    }
   }
+}
 
+void lostConnection()
+{
   Serial.print(count++);
   Serial.print(": ");
-  Serial.print(hourHigh);
-  Serial.print(hourLow);
+  Serial.print(hour());
   Serial.print(":");
-  Serial.print(minHigh);
-  Serial.print(minLow);
+  Serial.print(minute());
   Serial.print(", ");
   Serial.print(month());
   Serial.print("-");
@@ -491,39 +318,10 @@ void writeTime()
   Serial.println(year());
 }
 
-void setLocalTime()
-{
-  int tz = currentTimeZone();
-  int tz_offset = (tz * 60 * 60);
-
-  Serial.print("tz offset = ");
-  Serial.println(tz_offset);
-  setTime(Feather.getUtcTime() + tz_offset);
-}
-
-void setMicroTime()
-{
-  if (minute() == 0 && g_timeSet == false) {
-    setLocalTime();
-    g_timeSet = true;
-  }
-  else if (minute() != 0) {
-    g_timeSet = false;
-  }
-}
-
-void lostConnection()
-{
-  while (!Feather.connected()) {
-    digitalWrite(PA15, g_on);   // turn the LED on (HIGH is the voltage level)
-    g_on = !g_on;
-    Feather.connect(WLAN_SSID, WLAN_PASS);
-    delay(250);
-  }
-}
-
 void setup() 
 {
+  static WiFiEventHandler e1, e2;
+
   Serial.begin(115200);
   matrix1.begin(0x71);
   matrix2.begin(0x72);
@@ -541,12 +339,19 @@ void setup()
   matrix2.setTextColor(LED_ON);
   matrix3.setTextColor(LED_ON);
   matrix4.setTextColor(LED_ON);
-  
+
+  WiFi.begin("LivingRoom", "Motorazr2V8");
+
+  NTP.setTimeZone(CST_OFFSET);
+  NTP.onNTPSyncEvent([](NTPSyncEvent_t event) {
+    ntpEvent = event;
+    syncEventTriggered = true;
+  });
+
+  e1 = WiFi.onStationModeGotIP(onSTAGotIP);// As soon WiFi is connected, start NTP Client
+  e2 = WiFi.onStationModeDisconnected(onSTADisconnected);
+
   clearDisplay();
-  Feather.connect(WLAN_SSID, WLAN_PASS);
-  setLocalTime();
-  pinMode(PA15, OUTPUT);
-  digitalWrite(PA15, 1);
 
   lastMinLow = 99;
   lastMinHigh = 99;
@@ -556,14 +361,17 @@ void setup()
   g_on = false;
   g_timeSet = true;
   delay(2000);
-  setLocalTime();
   Serial.println("Done with setup");
 }
 
 void loop() 
 {
-  setMicroTime();
+  if (syncEventTriggered) {
+    processSyncEvent(ntpEvent);
+    syncEventTriggered = false;
+  }
+
   writeTime();
-  delay(1000 * 30);
+  delay(100);
 }
 
